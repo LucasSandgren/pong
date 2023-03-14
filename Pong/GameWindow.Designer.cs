f01c95d5584884ec2f5ba184321371f17b40f16c
Lucas Sandgren <101200129+LucasSandgren@users.noreply.github.com>
2023-03-15 00:42:09 +0100

    Add files via upload

---
+++ Pong/GameWindow.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Pong
+{
+    partial class GameWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameWindow));
+            this.PlayerScoreLabel = new System.Windows.Forms.Label();
+            this.PlayerPaddleModel = new System.Windows.Forms.PictureBox();
+            this.GameBall = new System.Windows.Forms.PictureBox();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.LifesRemaining = new System.Windows.Forms.Label();
+            this.livesRemainingText = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PlayerPaddleModel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GameBall)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PlayerScoreLabel
+            // 
+            this.PlayerScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.PlayerScoreLabel.Font = new System.Drawing.Font("Infinite Darkness", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlayerScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.PlayerScoreLabel.Location = new System.Drawing.Point(12, 9);
+            this.PlayerScoreLabel.Name = "PlayerScoreLabel";
+            this.PlayerScoreLabel.Size = new System.Drawing.Size(107, 18);
+            this.PlayerScoreLabel.TabIndex = 0;
+            this.PlayerScoreLabel.Text = "0";
+            // 
+            // PlayerPaddleModel
+            // 
+            this.PlayerPaddleModel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(211)))), ((int)(((byte)(211)))), ((int)(((byte)(65)))));
+            this.PlayerPaddleModel.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("PlayerPaddleModel.BackgroundImage")));
+            this.PlayerPaddleModel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.PlayerPaddleModel.Location = new System.Drawing.Point(159, 515);
+            this.PlayerPaddleModel.Name = "PlayerPaddleModel";
+            this.PlayerPaddleModel.Size = new System.Drawing.Size(63, 10);
+            this.PlayerPaddleModel.TabIndex = 1;
+            this.PlayerPaddleModel.TabStop = false;
+            // 
+            // GameBall
+            // 
+            this.GameBall.BackColor = System.Drawing.Color.Transparent;
+            this.GameBall.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("GameBall.BackgroundImage")));
+            this.GameBall.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.GameBall.InitialImage = ((System.Drawing.Image)(resources.GetObject("GameBall.InitialImage")));
+            this.GameBall.Location = new System.Drawing.Point(178, 483);
+            this.GameBall.Name = "GameBall";
+            this.GameBall.Size = new System.Drawing.Size(24, 25);
+            this.GameBall.TabIndex = 2;
+            this.GameBall.TabStop = false;
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 7;
+            this.GameTimer.Tick += new System.EventHandler(this.GameTick);
+            // 
+            // LifesRemaining
+            // 
+            this.LifesRemaining.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.LifesRemaining.BackColor = System.Drawing.Color.Transparent;
+            this.LifesRemaining.Font = new System.Drawing.Font("Infinite Darkness", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LifesRemaining.ForeColor = System.Drawing.Color.White;
+            this.LifesRemaining.Location = new System.Drawing.Point(350, 9);
+            this.LifesRemaining.Name = "LifesRemaining";
+            this.LifesRemaining.Size = new System.Drawing.Size(18, 18);
+            this.LifesRemaining.TabIndex = 3;
+            this.LifesRemaining.Text = "0";
+            this.LifesRemaining.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // livesRemainingText
+            // 
+            this.livesRemainingText.BackColor = System.Drawing.Color.Transparent;
+            this.livesRemainingText.Font = new System.Drawing.Font("Infinite Darkness", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.livesRemainingText.ForeColor = System.Drawing.Color.White;
+            this.livesRemainingText.Location = new System.Drawing.Point(257, 9);
+            this.livesRemainingText.Name = "livesRemainingText";
+            this.livesRemainingText.Size = new System.Drawing.Size(101, 18);
+            this.livesRemainingText.TabIndex = 4;
+            this.livesRemainingText.Text = "LIVES REMAINING:";
+            // 
+            // GameWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(380, 537);
+            this.Controls.Add(this.livesRemainingText);
+            this.Controls.Add(this.LifesRemaining);
+            this.Controls.Add(this.GameBall);
+            this.Controls.Add(this.PlayerPaddleModel);
+            this.Controls.Add(this.PlayerScoreLabel);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "GameWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GODPONG";
+            this.Load += new System.EventHandler(this.GameTick);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.GameWindow_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.GameWindow_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.PlayerPaddleModel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GameBall)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label PlayerScoreLabel;
+        private System.Windows.Forms.PictureBox PlayerPaddleModel;
+        private System.Windows.Forms.PictureBox GameBall;
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.Label LifesRemaining;
+        private System.Windows.Forms.Label livesRemainingText;
+    }
+}
---
+++ Pong/GameWindow.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pong
+{
+    public partial class GameWindow : Form
+    {
+        // Location Variables
+        int ballXCordinate = 5; // Used to determine ball speed
+        int ballYCordinate = 5;
+        
+
+        // Score Variables
+        int playerScore = 0; // Player Score
+        int playerLife = 3;
+        int bestScore = 0;
+        bool gameOver = false;
+
+        // Size Variables
+        int padelLeftBoundary; // Stops model from moving outside to the left
+        int padelRightBoundary; // Stops model from moving outside to the right
+        int bottomBoundary;
+        int centerPoint; // Center point of screen
+        int xMidPoint; // Center of X axis
+        int yMidPoint; // Center of Y axis
+        int leftWall;
+        int rightWall;
+
+        // Detection variables for player interactions
+        int spaceBarClick = 0;
+
+        // Model movement variables
+        float modelVelocity = 0;
+        float modelAcceleration = 6f;
+        float modelMaxVelocity = 6;
+
+        // SquareBox properties
+        const int BOX_WIDTH = 25;
+        const int BOX_HEIGHT = 25;
+        const int BOX_PADDING = 5;
+
+        public GameWindow()
+        {
+            InitializeComponent();
+            BoxGeneration();
+            
+            padelLeftBoundary = 0;
+            padelRightBoundary = ClientSize.Width;
+            xMidPoint = ClientSize.Width / 2;
+            yMidPoint = ClientSize.Height / 2;
+            centerPoint = xMidPoint + yMidPoint;
+            bottomBoundary = ClientSize.Height - PlayerPaddleModel.Bottom;
+            LifesRemaining.Text = playerLife.ToString();
+        }
+        // Create the boxes to hit with the ball to generate points
+        private void BoxGeneration()
+        {
+            const int BOX_NUM = 12;
+            const int BOX_ROWS = 3;
+            const int START_X = 13;
+            const int START_Y = 50;
+
+            Image[] BoxImages = {Properties.Resources.SquareBoxYellowNew, Properties.Resources.SquareBoxBlueNew, Properties.Resources.SquareBoxGreenNew, Properties.Resources.SquareBoxCracks};
+            for (int i = 0; i < BOX_ROWS; i++)
+            {
+                for (int j = 0; j < BOX_NUM; j++)
+                {
+                    PictureBox SquareBox = new PictureBox();
+                    SquareBox.Size = new Size(BOX_WIDTH, BOX_HEIGHT);
+                    SquareBox.Location = new Point(START_X + j * (BOX_WIDTH + BOX_PADDING), START_Y + i * (BOX_HEIGHT + BOX_PADDING));
+                    SquareBox.Tag = 2;
+                    SquareBox.SizeMode = PictureBoxSizeMode.CenterImage;
+                    SquareBox.Image = BoxImages[i];
+                    Controls.Add(SquareBox);
+                }
+            }
+        }
+
+        private void CheckCollision()
+        {
+            // Check for collision with player paddle
+            if (GameBall.Bounds.IntersectsWith(PlayerPaddleModel.Bounds)) {ballYCordinate = -ballYCordinate;}
+
+            // Check for collision with boxes
+            foreach (Control SquareBox in Controls)
+            {
+                if (SquareBox is PictureBox && SquareBox.Tag != null && (int)SquareBox.Tag > 0 && GameBall.Bounds.IntersectsWith(SquareBox.Bounds))
+                {
+                    // Decrement the box's health and reverse the direction of the ball
+                    SquareBox.Tag = (int)SquareBox.Tag - 1;
+                    ballYCordinate = -ballYCordinate;
+                    playerScore += 1;
+                    PlayerScoreLabel.Text = playerScore.ToString();
+
+                    // Remove the box if its health is zero
+                    if ((int)SquareBox.Tag == 0) {Controls.Remove(SquareBox);}
+
+                    /* // Shake the box NOT IMPLEMENTED/BUGGY
+                    int squareShakeDuration = 1;
+                    int squareshakeMagnitude = 1;
+                    Point originalPos = SquareBox.Location;
+                    Random rGenerator = new Random();
+                    for (int i = 0; i < squareShakeDuration; i++)
+                    {
+                        int dx = rGenerator.Next(-squareshakeMagnitude, squareshakeMagnitude);
+                        int dy = rGenerator.Next(-squareshakeMagnitude, squareshakeMagnitude);
+                        SquareBox.Location = new Point(originalPos.X + dx, originalPos.Y + dy);
+                        System.Threading.Thread.Sleep(20);
+                    }
+                    SquareBox.Location = originalPos;
+                    */
+                }
+            }
+        }
+        private void GameTick(object sender, EventArgs e)
+        {
+            CheckCollision(); 
+            // Pause game if PlayerLife reaches 0
+            if (gameOver == true) { GameTimer.Stop();
+                GameBall.Top = (ClientSize.Height - GameBall.Height) / 2;
+                GameBall.Left = (ClientSize.Width - GameBall.Width) / 2;
+                PlayerPaddleModel.Left = ClientSize.Width / 2 - 35;
+                return; }
+            // Ball Location    
+            GameBall.Top -= ballYCordinate;
+            GameBall.Left -= ballXCordinate;
+            
+            // Check if ball has gone below bottom screen
+            if (GameBall.Top > ClientSize.Height)
+            {
+                GameBall.Top = xMidPoint;
+                ballXCordinate = -ballXCordinate;
+                playerLife--;
+                LifesRemaining.Text = playerLife.ToString();
+                if (playerLife <= 0) {if (playerScore > bestScore) {playerScore = bestScore;} gameOver = true;}
+            }
+            // Check if ball hits left or right wall
+            if (GameBall.Left < 0 || GameBall.Right > ClientSize.Width) {ballXCordinate = -ballXCordinate;}
+
+            // Check if ball hits top wall
+            if (GameBall.Top < 0) {ballYCordinate = -ballYCordinate;}
+
+            // Model movement implementation
+            PlayerPaddleModel.Left += (int)modelVelocity;
+
+            // Prevent model from going beyond screen edges
+            if (PlayerPaddleModel.Left < padelLeftBoundary) {PlayerPaddleModel.Left = padelLeftBoundary; modelVelocity = 0;}
+            else if (PlayerPaddleModel.Right > padelRightBoundary)
+            {PlayerPaddleModel.Left = padelRightBoundary - PlayerPaddleModel.Width; modelVelocity = 0;}
+            // Apply velocity
+            modelVelocity = Math.Max(Math.Min(modelVelocity, modelMaxVelocity), -modelMaxVelocity);
+        }
+
+        private void GameWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {   // Model movement implementation
+                if (PlayerPaddleModel.Left > padelLeftBoundary) {modelVelocity -= modelAcceleration;}
+            }
+            else if (e.KeyCode == Keys.Right)
+            {   //Model movement implementation
+                if (PlayerPaddleModel.Right < ClientSize.Width) { modelVelocity += modelAcceleration;} 
+            }
+        }
+
+        private void GameWindow_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.Right)
+            {
+                // Decelerate paddle when the key is released
+                if (modelVelocity >= 0) {modelVelocity = 0;}
+                else if (modelVelocity < 0) {modelVelocity = 0;}
+            }
+        }
+
+        
+    }
+}
+
